ba8a0f7dd699da7f87328ce7237593440a53441d
Balkarn <57596334+Balkarn@users.noreply.github.com>
2020-07-24 19:40:23 +0100

    changed endpoint for heroku hosted server

--- src/clientcomponent.js
+++ src/clientcomponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001";
+const ENDPOINT = "https://scribblechat-server.herokuapp.com/";
 
 export default function ClientComponent() {
   const [response, setResponse] = useState("");

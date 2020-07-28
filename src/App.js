5907daa1ab314a1ef4372f1867999424b03002a5
Balkarn <57596334+Balkarn@users.noreply.github.com>
2020-07-28 14:21:33 +0100

    deleted old code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
-import ClientComponent from "./clientcomponent.js";
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
 
-function App() {
-  const [loadClient, setLoadClient] = useState(true);
+const ENDPOINT = "http://127.0.0.1:4001"; //for local testing
+//const ENDPOINT = "https://scribblechat-server.herokuapp.com/"; //for heroku testing
+
+const socket = socketIOClient(ENDPOINT);
+
+export default function ClientComponent() {
+  const [response, setResponse] = useState("");
+  const [responses, setResponses] = useState([]);
+
+  useEffect(() => {
+    socket.on("FromAPI", data => {
+      setResponse(data);
+      setResponses(responses => [...responses, data]);
+    });
+
+    return () => socket.disconnect(); //close connection when component unmounts
+
+  }, []);
+
   return (
-    <>
-      {/* LOAD OR UNLOAD THE CLIENT */}
-      <button onClick={() => setLoadClient(prevState => !prevState)}>
-        TOGGLE CLIENT
-      </button>
-      {/* SOCKET IO CLIENT*/}
-      {loadClient ? <ClientComponent /> : null}
-    </>
+    <ul>
+      {responses.map(aResponse => (
+        <li>{aResponse}</li>
+      ))}
+    </ul>
   );
 }
-
-export default App;
